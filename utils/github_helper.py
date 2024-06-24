6c9c75bbec62c34f1a10f1d8e3baa930f7e0b1b8
HUZAIFAJUZER\Huzaifa <huzaifa.juzer@techlogix.com>
2024-06-24 22:56:09 +0500

    Updated github_helper

--- utils/github_helper.py
+++ utils/github_helper.py
@@ -48,15 +48,16 @@ def process_directory(contents_url):
     contents = get_repo_contents(contents_url)
     for content in contents:
         if content['type'] == 'file':
+            # Remove extension from filename if it exists
+            file_name = os.path.splitext(content['name'])[0] + '.txt'
             file_content = get_file_content(content['url'])
-            # Append '.txt' extension and ensure forward slashes
-            file_name = content['name'] + '.txt'
             file_path = os.path.join(base_dir, file_name).replace('\\', '/')
             save_file(file_path, file_content)
             print(f"Saved file: {file_path}")
         elif content['type'] == 'dir':
-            new_dir = os.path.join( content['name']).replace('\\', '/')
+            # Continue processing recursively for directories
             process_directory(content['url'])
+
 
 # URL to the repository contents
 repo_url = f'https://api.github.com/repos/{username}/{repository}/contents'
